864a985baa61b1bcbc664fff16cc1fa3857926af
Jonathan Atkins <jonathanjatkins89@gmail.com>
2026-07-15 16:15:35 -0700

    creating page components

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return <h1>Login Page</h1>;
 }
 
