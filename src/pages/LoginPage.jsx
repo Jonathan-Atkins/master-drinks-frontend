e424fc25b1b6907166553c684acfefdcec528374
Jonathan Atkins <jonathanjatkins89@gmail.com>
2026-07-15 21:16:44 -0700

    added login functions and registerpage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,8 +4,55 @@ import { API_URL } from "../config/api";
 function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-  return <h1>Login Page</h1>;
+    const response = await fetch(`${API_URL}/api/v1/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  return (
+    <main>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+
+        <button type="submit">Log In</button>
+      </form>
+    </main>
+  );
 }
 
 export default LoginPage;
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,4 +1,4 @@
-import { API_URL } from "../config/api"
+import { API_URL } from "../config/api";
 
 function UsersPage() {
   return <h1>Login Page</h1>;
